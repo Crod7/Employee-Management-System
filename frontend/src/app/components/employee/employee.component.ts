5558c33629ef027dc892ae999c179eca632ae1d1
Carlos Rodriguez <crodriguezyt10@gmail.com>
2024-02-11 22:22:26 -0800

    EPMG-[011] Roles are now called into application through api call.

--- frontend/src/app/components/employee/employee.component.ts
+++ frontend/src/app/components/employee/employee.component.ts
@@ -5,46 +5,64 @@ import { Role } from '@app/models/Role';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-employee',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+    selector: 'app-employee',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './employee.component.html',
+    styleUrl: './employee.component.css'
 })
 export class EmployeeComponent implements OnInit {
-  data: any;
+    empData: any;
+    rolesData: any;
 
-  employees: Employee[] = [];
+    roles: Role[] = [];
+    employees: Employee[] = [];
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.getAllEmployees();
-  }
-  getAllEmployees() {
-    const getAllEmployeesUrl = 'https://epmgapi.azurewebsites.net/api/employees';
+    ngOnInit() {
+        this.getAllRoles()
+        this.getAllEmployees();
+    }
+    getAllRoles() {
+        const getAllRolesUrl = 'https://epmgapi.azurewebsites.net/api/role';
 
-    this.http.get(getAllEmployeesUrl).subscribe((getAllEmployeeResponse) => {
-      this.data = getAllEmployeeResponse;
-      console.log(this.data); // Logs the fetched data(array)
+        this.http.get(getAllRolesUrl).subscribe((getAllRolesResponse) => {
+            this.rolesData = getAllRolesResponse;
+            console.log(this.rolesData)
 
-      // We map each employee to employees arrray
-      this.employees = this.data.map((employeeData: any) => {
-        return {
-          employeeId: employeeData.employeeId,
-          name: employeeData.name,
-          email: employeeData.email,
-          phone: employeeData.phone,
-          address: employeeData.address,
-          city: employeeData.city,
-          state: employeeData.state,
-          postalCode: employeeData.postalCode,
-          roleId: employeeData.roleId,
-          payGradeId: employeeData.payGradeId,
-          personalInfoId: employeeData.personalInfoId,
-          scheduleId: employeeData.scheduleId
-        } as Employee;
-      })
-    });
-  }
+            this.roles = this.rolesData.map((roleData: any) => {
+                return {
+                    roleId: roleData.roleId,
+                    roleName: roleData.name
+                }
+            })
+        })
+    }
+    getAllEmployees() {
+        const getAllEmployeesUrl = 'https://epmgapi.azurewebsites.net/api/employees';
+
+        this.http.get(getAllEmployeesUrl).subscribe((getAllEmployeeResponse) => {
+            this.empData = getAllEmployeeResponse;
+            console.log(this.empData); // Logs the fetched data(array)
+
+            // We map each employee to employees arrray
+            this.employees = this.empData.map((employeeData: any) => {
+                return {
+                    employeeId: employeeData.employeeId,
+                    name: employeeData.name,
+                    email: employeeData.email,
+                    phone: employeeData.phone,
+                    address: employeeData.address,
+                    city: employeeData.city,
+                    state: employeeData.state,
+                    postalCode: employeeData.postalCode,
+                    roleId: employeeData.roleId,
+                    payGradeId: employeeData.payGradeId,
+                    personalInfoId: employeeData.personalInfoId,
+                    scheduleId: employeeData.scheduleId
+                } as Employee;
+            })
+        });
+    }
 }
