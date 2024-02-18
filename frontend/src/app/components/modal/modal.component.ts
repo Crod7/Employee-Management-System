fb16660eb84112790a441295feb8b6aa43a7645b
Carlos Rodriguez <crodriguezyt10@gmail.com>
2024-02-18 00:57:44 -0800

    EPMG-[014] Modal box created.

--- frontend/src/app/components/modal/modal.component.ts
+++ frontend/src/app/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-modal',
@@ -9,6 +10,14 @@ import { CommonModule } from '@angular/common';
     styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+    modalForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.modalForm = this.fb.group({
+            input1: ['', Validators.required],
+            input2: ['', Validators.pattern(/^[A-Za-z0-9]+$/)]
+        });
+    }
     @Output() closeModal = new EventEmitter<void>();
 
     onClose() {
