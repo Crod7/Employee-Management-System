5ba2f2878876bfc8a871e60a421a303c9abadada
Carlos Rodriguez <crodriguezyt10@gmail.com>
2024-02-24 02:18:22 -0800

    EPMG-[014] Added FormControl to grab values from input fields and print them on to the console.

--- frontend/src/app/components/modal/modal.component.ts
+++ frontend/src/app/components/modal/modal.component.ts
@@ -1,42 +1,59 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-modal',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './modal.component.html',
     styleUrl: './modal.component.css'
 })
 export class ModalComponent {
-    modalForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {
-        this.modalForm = this.fb.group({
-            requiredField: ['', Validators.required],
-            nonRequiredField: [''],
-            phonePart1: ['', [Validators.required, Validators.maxLength(3), Validators.pattern('^[0-9]+$')]],
-            phonePart2: ['', [Validators.required, Validators.maxLength(3), Validators.pattern('^[0-9]+$')]],
-            phonePart3: ['', [Validators.required, Validators.maxLength(4), Validators.pattern('^[0-9]+$')]]
-        });
+    employeeForm = new FormGroup({
+        name: new FormControl(''),
+        email: new FormControl(''),
+        phonePart1: new FormControl(''),
+        phonePart2: new FormControl(''),
+        phonePart3: new FormControl(''),
+        address1: new FormControl(''),
+        address2: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        zipcode: new FormControl('')
+    });
+
+    submitApplication(
+        name: string,
+        email: string,
+        phonePart1: string,
+        phonePart2: string,
+        phonePart3: string,
+        address1: string,
+        address2: string,
+        city: string,
+        state: string,
+        zipcode: string
+    ) {
+        console.log(name)
     }
 
-
-    ngOnInit() {
-        this.modalForm = this.fb.group({
-            name: ['', Validators.required],
-            email: ['', Validators.required],
-            phonePart1: [''],
-            phonePart2: [''],
-            phonePart3: [''],
-            address1: [''],
-            address2: [''],
-            city: [''],
-            state: [''],
-            zipcode: ['']
-        });
+    employeeAddSubmit() {
+        this.submitApplication(
+            this.employeeForm.value.name ?? '',
+            this.employeeForm.value.email ?? '',
+            this.employeeForm.value.phonePart1 ?? '',
+            this.employeeForm.value.phonePart2 ?? '',
+            this.employeeForm.value.phonePart3 ?? '',
+            this.employeeForm.value.address1 ?? '',
+            this.employeeForm.value.address2 ?? '',
+            this.employeeForm.value.city ?? '',
+            this.employeeForm.value.state ?? '',
+            this.employeeForm.value.zipcode ?? ''
+        )
     }
+
     @Output() closeModal = new EventEmitter<void>();
 
     onClose() {
